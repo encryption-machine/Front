e2b11c31c514bba57dbb1b746bf6a76bd5db714d
unknown <cbeta09@list.ru>
2023-06-12 19:46:15 +0500

    fix: перенесла из app в main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Routers } from '../../services/Routers';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
 	return (
 		<div className="App">
-      <AboutProject/>
 			<Router>
 				<Routers />
 			</Router>
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import AboutProject from '../../src/components/AboutProject/AboutProject';
 
 export const MainPage = () => {
-	return <div className="layout">здесь будет контент главной</div>;
+	return <div className="layout">
+    <AboutProject/>
+  </div>;
 };
