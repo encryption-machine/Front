dcb0042e245c457c11598ea4f65947d523e6c64d
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-06-27 03:46:38 +0300

    fix/AboutCiphers

--- src/components/AboutCiphers/AboutCiphers.jsx
+++ src/components/AboutCiphers/AboutCiphers.jsx
@@ -1,18 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
 import { ciphersInfo } from './AboutCiphers.constants';
 import styles from './AboutCiphers.module.scss';
+import SvgSelector from '../SvgSelector/SvgSelector';
+import CheckMark from '../CheckMark/CheckMark';
 
 const AboutCiphers = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const cipherRef = useRef(null);
+
+  const handleOpen = (event, id) => {
+    event.preventDefault();
+    // console.log(cipherRef.current);
+    console.log(id);
+    console.log(event.currentTarget.id);
+    console.log(cipherRef.current);
+    setIsOpen(!isOpen);
+  };
+
   return (
     <section className={styles.aboutCiphers} id="aboutCiphers">
       <h2 className={styles.aboutCiphers_title}>О шифрах</h2>
-      <div>
+      <ul>
         {ciphersInfo.map(({ id, name, description }) => (
-          <div className={styles.aboutCiphers_cipher} key={id}>
-            <h3 className={styles.aboutCiphers_name}>{name}</h3>
-            <p className={styles.aboutCiphers_description}>{description}</p>
-          </div>
+          <li 
+            ref={cipherRef} className={styles.aboutCiphers_ciphers}
+            key={id} onClick={e=>handleOpen(e, id)}
+          >
+            <div  className={styles.aboutCiphers_cipher} >
+              <div className={styles.aboutCiphers_svg}>
+                <SvgSelector name={name} />
+              </div>
+              <h3 className={styles.aboutCiphers_name}>{name}</h3>
+              <div className={styles.aboutCiphers_svg} >
+                <CheckMark />
+              </div>
+            </div>
+            {isOpen &&  (
+              <p className={styles.aboutCiphers_description}>{description}</p>
+            )}
+          </li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 };
