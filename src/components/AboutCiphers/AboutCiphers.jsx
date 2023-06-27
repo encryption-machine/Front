118c817c696b066abea7ef046d625e1b51a57419
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-06-27 10:10:12 +0300

    fix/add handleOpen

--- src/components/AboutCiphers/AboutCiphers.jsx
+++ src/components/AboutCiphers/AboutCiphers.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { ciphersInfo } from './AboutCiphers.constants';
 import styles from './AboutCiphers.module.scss';
 import SvgSelector from '../SvgSelector/SvgSelector';
 import CheckMark from '../CheckMark/CheckMark';
 
 const AboutCiphers = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const cipherRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(null);
 
-  const handleOpen = (event, id) => {
-    event.preventDefault();
-    // console.log(cipherRef.current);
-    console.log(id);
-    console.log(event.currentTarget.id);
-    console.log(cipherRef.current);
-    setIsOpen(!isOpen);
+  const handleOpen = (id) => {
+    if (isOpen === id) {
+      return setIsOpen(null);
+    }
+    setIsOpen(id);
   };
 
   return (
@@ -22,22 +19,30 @@ const AboutCiphers = () => {
       <h2 className={styles.aboutCiphers_title}>О шифрах</h2>
       <ul>
         {ciphersInfo.map(({ id, name, description }) => (
-          <li 
-            ref={cipherRef} className={styles.aboutCiphers_ciphers}
-            key={id} onClick={e=>handleOpen(e, id)}
+          <li
+            className={styles.aboutCiphers_ciphers}
+            key={id}
+            onClick={() => handleOpen(id)}
           >
-            <div  className={styles.aboutCiphers_cipher} >
+            <div className={styles.aboutCiphers_cipher}>
               <div className={styles.aboutCiphers_svg}>
                 <SvgSelector name={name} />
               </div>
               <h3 className={styles.aboutCiphers_name}>{name}</h3>
-              <div className={styles.aboutCiphers_svg} >
+              <div className={styles.aboutCiphers_svg}>
                 <CheckMark />
               </div>
             </div>
-            {isOpen &&  (
-              <p className={styles.aboutCiphers_description}>{description}</p>
-            )}
+
+            <p
+              className={
+                isOpen === id
+                  ? styles.aboutCiphers_description_show
+                  : styles.aboutCiphers_description
+              }
+            >
+              {description}
+            </p>
           </li>
         ))}
       </ul>
