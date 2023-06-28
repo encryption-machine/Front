8d41a42a84206dcf189dc08921c1d5793baa37f1
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-06-28 16:54:32 +0300

    fix/add accordion

---
+++ src/components/Accordion/AccordionItem/AccordionItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SvgSelector from '../../SvgSelector/SvgSelector';
+import CheckMark from '../../CheckMark/CheckMark';
+import styles from './AccordionItem.module.scss';
+
+export default function accordionItem({
+  id,
+  name,
+  description,
+  isActiveSection,
+  setActiveIndex,
+  sectionIndex,
+}) {
+  const toggleAccordionItem = () => {
+    const openIndex = isActiveSection ? null : sectionIndex;
+    setActiveIndex(openIndex);
+  };
+  return (
+    <li
+      className={styles.accordionItem_ciphers}
+      key={id}
+      onClick={toggleAccordionItem}
+    >
+      <div className={styles.accordionItem_cipher}>
+        <div className={styles.accordionItem_svg}>
+          <SvgSelector name={name} />
+        </div>
+        <h3 className={styles.accordionItem_name}>{name}</h3>
+        <div className={styles.accordionItem_svg}>
+          <CheckMark />
+        </div>
+      </div>
+      {isActiveSection ? (
+        <p className={styles.accordionItem_description_show}>{description}</p>
+      ) : (
+        <p className={styles.accordionItem_description}>{description}</p>
+      )}
+    </li>
+  );
+}
