4c324a6951699ebc7ea5313cd9af6af22353c378
Maria Gromyko <mazvereva91@mail.ru>
2023-07-01 11:35:45 +0300

    fix:bugs after build

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className={styles.footer_container}>
         <ul className={styles.list}>
           <li>
-            <a href="tel:8(800)234-12-36">8(800)234-12-36</a>
+            <a href="#">8(800)234-12-36</a>
           </li>
           <li>
             <a href="#" target="_blank">
