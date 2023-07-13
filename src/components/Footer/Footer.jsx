320029442274f49557e48ce4bf700de075948b3a
Maria Gromyko <mazvereva91@mail.ru>
2023-07-13 12:46:52 +0300

    fix: add tel in constants

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import styles from './Footer.module.scss';
+import { telephone } from '../../constants/contacts';
 
 export const Footer = () => {
+  console.log(telephone)
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
         <ul className={styles.list}>
           <li>
-            <a href="tel:8(800)234-12-36">8(800)234-12-36</a>
+            <a href="tel:telephone">{telephone}</a>
           </li>
           <li>
-            <a href="#" target="_blank">
+            <a href="#chipers" target="_blank">
               Политика конфиденциальности
             </a>
           </li>
