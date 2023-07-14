2d4bb5a0e17d1525584791d6b9c02d46023bf505
Maria Gromyko <mazvereva91@mail.ru>
2023-07-14 10:45:20 +0300

    fix: deleted privacy policy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,7 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
-        <ul className={styles.list}>
-          <li>
-            <p className={styles.telephone}>{telephone}</p>
-          </li>
-          <li>
-            <a href="#chipers" target="_blank">
-              Политика конфиденциальности
-            </a>
-          </li>
-        </ul>
+        <p className={styles.telephone}>{telephone}</p>
       </div>
     </footer>
   );
