7f326f760037d440962c1eaf4924429a453cebd0
Maria Gromyko <mazvereva91@mail.ru>
2023-07-18 14:07:40 +0300

    feat: layout for authorized and non-authorized user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,36 @@
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-import { Link } from 'react-router-dom';
+import { FormGlobalStore as formStore } from '../../stores/';
+import styles from './Header.module.scss';
 import AuthModal from '../AuthModal/AuthModal';
 import AuthTabs from '../AuthTabs/AuthTabs';
 import RecoveryPasswordForm from '../RecoveryPasswordForm/RecoveryPasswordForm';
-import { FormGlobalStore as formStore } from '../../stores/';
-import styles from './Header.module.scss';
-import logotype from '../../assets/icons/logotype.svg';
 import { CustomLink } from '../CustomLink/CustomLink';
-import { useState } from 'react';
+import logotype from '../../assets/icons/logotype.svg';
 
 export const Header = observer(() => {
-
+  // временное решение, пока не реализовано апи авторизации
   const [loggedIn, setLogedIn] = useState(true);
+
+  const location = useLocation();
 
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
   };
 
-
-
   return (
     <header className={styles.header}>
       <div className={styles.header_container}>
-        <img className={styles.logotype} src={logotype} alt="logo" />
+        <CustomLink
+          href={"/"}
+          target="_self"
+        >
+          <img className={styles.logotype} src={logotype} alt="logo" />
+        </CustomLink>
         <nav>
-          <ul className={styles.list}>
+          {(location.pathname === '/') && <ul className={styles.list}>
             <li className={styles.chapter}>
               <CustomLink
                 href={'#ciphers'}
@@ -55,23 +60,26 @@ export const Header = observer(() => {
             </li>
             {loggedIn &&
               <li>
-                <Link
-                  to='/profile'
+                <CustomLink
+                  href={'/profile'}
+                  target="_self"
                 >
                   Личный&nbsp;кабинет
-                </Link>
+                </CustomLink>
               </li>
 
             }
-          </ul>
+          </ul>}
         </nav>
         <div className={styles.entrance}>
+          {(location.pathname === '/profile') && <span className={styles.email}>user@email.ru</span>}
           <button
             className={styles.button_header}
             type="button"
             onClick={() => formStore.setOpenAuthForm(true)}
           >
-            Войти
+            {/* временное решение пока нет авторизации */}
+            {loggedIn ? 'Выйти' : 'Войти'}
           </button>
           <AuthModal
             isOpen={formStore.openAuthForm}
