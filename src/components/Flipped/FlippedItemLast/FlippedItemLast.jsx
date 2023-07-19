6e7a46c58e43ff23a8e6802ac65a824cad528273
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-07-19 14:05:36 +0300

    fix FlippedItemLast

--- src/components/Flipped/FlippedItemLast/FlippedItemLast.jsx
+++ src/components/Flipped/FlippedItemLast/FlippedItemLast.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import styles from './FlippedItemLast.module.scss';
 
 export default function FlippedItemLast() {
-  return (
-    <div className={styles.flippedItemLast}>
-      <div className={styles.flippedItemLast_svg}></div>
-    </div>
-  );
+  return <div className={styles.flippedItemLast}></div>;
 }
