cd98034543592cc46304ebb3e5e93f7673c4ef9e
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-07-29 15:48:54 +0300

    fix cookieModal

--- src/components/CookiePopup/CookiePopup.jsx
+++ src/components/CookiePopup/CookiePopup.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable react/jsx-pascal-case */
 import { Modal } from '../Modal/Modal';
 import styles from './CookiePopup.module.scss';
-import { getCookie, setCookie } from '../../utils/cookie';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const CookiePopup = () => {
-  const cookieModal = getCookie('cookieModal');
-  console.log(cookieModal);
-  const setCookieModal = () => {
-    setCookie('cookieModal', true);
-    window.location.reload();
-  };
+  const [isShow, setIsShow] = useState(
+    localStorage.cookieModal ? JSON.parse(localStorage.cookieModal) : true
+  );
+  useEffect(() => {
+    localStorage.setItem('cookieModal', JSON.stringify(isShow));
+  }, [isShow]);
 
   return (
     <>
-      {!cookieModal && (
+      {isShow && (
         <Modal.window className={styles.container}>
           <p className={styles.text__primary}>
             Мы используем
             <span className={styles.text__secondary}>&nbsp;сookies-файлы</span>
           </p>
-          <Modal.close className={styles.close} onClose={setCookieModal}>
+          <Modal.close className={styles.close} onClose={setIsShow}>
             ok
           </Modal.close>
         </Modal.window>
