369fe99078b64ce0b610d83a5f9f8e1afa136999
Maria Gromyko <mazvereva91@mail.ru>
2023-08-12 15:37:20 +0300

    feat: create telegram btn

--- src/layouts/main-layout/index.jsx
+++ src/layouts/main-layout/index.jsx
@@ -3,6 +3,7 @@ import { Footer } from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import CookiePopup from '../../components/CookiePopup/CookiePopup';
+import TelegramButton from '../../components/TelegramButton/TelegramButton';
 
 const MainLayout = () => {
   return (
@@ -13,6 +14,7 @@ const MainLayout = () => {
       </section>
       <Footer />
       <CookiePopup />
+      <TelegramButton />
     </div>
   );
 };
